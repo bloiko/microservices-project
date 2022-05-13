df24f7138adbb63e341eaeb7091c5285fc684420
Loiko <bohdan.loiko@gmail.com>
2022-05-13 10:23:13 +0300

    create one server and three clients

--- business-services/one/src/main/java/org/example/ServoServerApplication.java
+++ business-services/one/src/main/java/org/example/ServoServerApplication.java
@@ -2,10 +2,11 @@ package org.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaServer
+@EnableDiscoveryClient
 public class ServoServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServoServerApplication.class, args);
--- business-services/two/src/main/java/example/ServoServerApplication.java
+++ business-services/two/src/main/java/example/ServoServerApplication.java
@@ -2,10 +2,10 @@ package example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableEurekaServer
+@EnableDiscoveryClient
 public class ServoServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServoServerApplication.class, args);
